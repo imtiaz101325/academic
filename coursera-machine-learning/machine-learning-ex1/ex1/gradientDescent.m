38bb482ed78c2191ff577db511058518338b1532
excalliburbd <excalliburbd@gmail.com>
2016-10-17 23:10:17 +0600

    gradient desent

--- coursera-machine-learning/machine-learning-ex1/ex1/gradientDescent.m
+++ coursera-machine-learning/machine-learning-ex1/ex1/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -11,21 +11,23 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
 
+    hypothesis = X * theta;
+    error_vector = hypothesis - y;
+    theta_change = alpha * (1/m) * (X' * error_vector);
 
 
-
-
+    theta = theta - theta_change;
 
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
 
 end
